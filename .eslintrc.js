64c45216c02a0b1eb2b6ad544d56d54074652b53
Ben Demboski <ben@turbopatent.com>
2018-01-07 00:31:50 -0800

    First few sections

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,6 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
-  },
-  extends: 'eslint:recommended',
-  env: {
-    browser: true
-  },
-  rules: {
-  }
+  extends: [
+    'plugin:turbopatent/ember'
+  ]
 };
