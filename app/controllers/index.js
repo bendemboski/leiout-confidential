2e87b87dd4363ca2ad7420700a13dcac7d51ec6b
Ben Demboski <ben@turbopatent.com>
2018-01-07 22:28:38 -0800

    Add more content

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,6 +4,7 @@ export default Controller.extend({
   urls: Object.freeze({
     throwback: "http://lathrowback.org/",
     whyThrowback: "https://lathrowback.org/why-throwback/",
+    laWeekly: "http://www.laweekly.com/",
     leioutBrackets: "http://leiout.com/sites/default/files/OFFICIAL_Lei_Out_2017_Schedule_Printable_2017-01-17.pdf",
     redditPermits: "https://www.reddit.com/r/ultimate/comments/5pjwsp/can_we_talk_about_the_ridiculous_morning_at_lei/dcw8md9/",
     leioutFaq: "https://leiout.com/news/lei-out-faq",
@@ -16,6 +17,11 @@ export default Controller.extend({
     familyActionPag: "http://www.familyactionpac.org/mission/",
     pacificResearch: "https://www.pacificresearch.org/mission-statement/",
     laWeeklySnopes: "https://www.snopes.com/2017/12/01/los-angeles-alt-weekly-sale/",
-    laWeeklyDeadspin: "https://theconcourse.deadspin.com/rich-people-demolished-l-a-weekly-to-build-the-future-1821019602"
+    laWeeklyDeadspin: "https://theconcourse.deadspin.com/rich-people-demolished-l-a-weekly-to-build-the-future-1821019602",
+    laoutMinutesAugust2016: "http://laout.org/wp-content/uploads/Board-Minutes_082216-1.pdf",
+    laoutMinutesMay2017: "http://laout.org/wp-content/uploads/2017_05_15-public.pdf",
+    laoutMinutesAugust2017: "http://laout.org/wp-content/uploads/2017_08_21-public.pdf",
+    laoutBoard: "http://laout.org/board/contact/",
+    tinoTran: "http://www.tinotran.com/main/"
   })
 });
